f8f7cefba11c2d9f5d8095711cde5579ee882c16
luciomotta <lucio-hto@hotmail.com>
2026-06-07 17:38:36 -0300

    Atualiza configuração para usar adapter-netlify e adiciona arquivo netlify.toml

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		// O adapter-netlify cuidará de toda a configuração do servidor
+		adapter: adapter()
+	}
 };
 
 export default config;
